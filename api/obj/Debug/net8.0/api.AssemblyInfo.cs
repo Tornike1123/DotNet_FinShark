6b6027539893911756d065ed40a94996c86bf726
Tornike Khatiashvili <95305738+Tornike1123@users.noreply.github.com>
2024-09-24 14:35:52 +0400

    Controllers

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff069696bf44d5a174333461ebf8217a34154113")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81a0d2fd34d4b25ae41fa7026b549e837ea9b137")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
