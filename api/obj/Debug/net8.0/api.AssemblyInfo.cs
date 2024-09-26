7da0e12c4547ea93fd4a279c0e3399bdc6335e01
Tornike Khatiashvili <95305738+Tornike1123@users.noreply.github.com>
2024-09-26 14:29:48 +0400

    POST method

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6607d2aed697f26c9d230b1714d13a7ad389e5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34df5c4886c09271bfd5324ad8115ab4c615b5c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
