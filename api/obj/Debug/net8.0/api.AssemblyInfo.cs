2e47736049f8635961e500642ca775df4c49277b
Tornike Khatiashvili <95305738+Tornike1123@users.noreply.github.com>
2024-09-30 13:13:43 +0400

    PUT(update)

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7da0e12c4547ea93fd4a279c0e3399bdc6335e01")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7f09bbda24370396165557e5000af486d42ab83")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
